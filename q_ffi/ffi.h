c7a07b801f378b8520fc1010453469f16349618e
Freddie Wu <freddie.wu@capitaledge.cn>
2020-09-05 17:48:23 +0800

    separate extern from export declaration

--- q_ffi/ffi.h
+++ q_ffi/ffi.h
@@ -3,6 +3,8 @@
 
 #include "k_compat.h"
 
-q_ffi_API K K4_DECL load(K dllSym, K fName, K resType, K parTypes);
+q_ffi_EXTERN q_ffi_API
+K K4_DECL load(K dllSym, K fName, K resType, K parTypes);
 
-q_ffi_API K K4_DECL version(K /*2: requires >= 1 arg*/);
+q_ffi_EXTERN q_ffi_API
+K K4_DECL version(K /*2: requires >= 1 arg*/);
