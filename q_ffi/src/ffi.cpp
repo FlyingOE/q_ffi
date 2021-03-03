689b7f6dfff68fc6d917e526fbd960cad562e8fe
Freddie Wu <freddie.wu@capitaledge.cn>
2021-03-03 10:48:42 +0800

    debug VS GTest integration issue

--- q_ffi/src/ffi.cpp
+++ q_ffi/src/ffi.cpp
@@ -1,14 +1,45 @@
 #include "version.hpp"
+#include "ktypes.hpp"
 #include "ffi.h"
-#include <dlfcn.h>
+//#include <dlfcn.h>
 
+#if WIN32
+BOOL APIENTRY DllMain(HMODULE /*hModule*/,
+    DWORD  ul_reason_for_call,
+    LPVOID /*lpReserved*/
+)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+#endif
+
+#include <iostream>
 ::K K4_DECL load(::K dllSym, ::K fName, ::K resType, ::K parTypes)
 {
 	dllSym++;
 	fName++;
 	resType++;
 	parTypes++;
+    return ::krr("error!");
+    /*
+    auto handle = dlopen("QQQQQ:kernel32.dll", RTLD_NOW);
+    std::cout << handle << std::endl;
+    if (!handle) {
+        return q::error("failed to load kernel32.dll", true);
+    }
+    else {
+        dlclose(handle);
+    }
 	return version(nullptr);
+    */
 }
 
 ::K K4_DECL version(K)
