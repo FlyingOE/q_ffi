48965f5048870db0be9b441e7d1ebf441d74a942
Freddie Wu <freddie.wu@capitaledge.cn>
2021-03-18 10:01:13 +0800

    test integrating libffi v3.3

--- q_ffi/src/ffi.cpp
+++ q_ffi/src/ffi.cpp
@@ -1,7 +1,8 @@
 #include "ktype_traits.hpp"
 #include "ffi.h"
-#include <dlfcn.h>
 #include "version.hpp"
+#include <dlfcn.h>
+#include <ffi.h>
 
 #include <iostream>
 ::K K4_DECL load(::K dllSym, ::K fName, ::K resType, ::K parTypes)
