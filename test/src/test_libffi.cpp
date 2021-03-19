fdf9cfdcc816784617f214bcbea6a6b4f1936c3e
Freddie Wu <freddie.wu@capitaledge.cn>
2021-03-19 23:58:45 +0800

    add libffi unit test for Windows x64

--- test/src/test_libffi.cpp
+++ test/src/test_libffi.cpp
@@ -73,18 +73,24 @@ TEST(libffiBaseTests, Win32API)
 
     ffi_cif cif{};
     ffi_type* args[1] = { &ffi_type_sint };
-    auto const status = ffi_prep_cif(&cif, FFI_STDCALL, 1, &ffi_type_sint, args);
+#ifdef _WIN64
+    ffi_abi const abi = FFI_WIN64;
+#else
+    ffi_abi const abi = FFI_STDCALL;
+#endif
+    auto const status = ffi_prep_cif(&cif, abi, 1, &ffi_type_sint, args);
     ASSERT_EQ(status, FFI_OK);
 
-    ASSERT_GE(sizeof(int), sizeof(ffi_arg)) << "Use <int> to receive result";
     int widthQuery = SM_CXSCREEN;
     int heightQuery = SM_CYSCREEN;
-    int width{ 0 }, height{ 0 };
+    ffi_sarg width0, height0;
     void* params[1] = { &widthQuery };
-    ffi_call(&cif, fp, &width, params);
+    ffi_call(&cif, fp, &width0, params);
     params[0] = &heightQuery;
-    ffi_call(&cif, fp, &height, params);
+    ffi_call(&cif, fp, &height0, params);
 
+    int const width = *reinterpret_cast<int*>(&width0);
+    int const height = *reinterpret_cast<int*>(&height0);
     std::cout << "Current screen resolution = " << width << " x " << height << std::endl;
     EXPECT_GE(width, 640);
     EXPECT_GE(height, 480);
@@ -124,10 +130,15 @@ namespace
     struct function_traits;
 
     template<class R, class... Args>
-    struct function_traits<R(__cdecl*)(Args...)>
+    struct function_traits<R(__cdecl *)(Args...)>
         : public function_traits<R(Args...)>
     {
-        static constexpr ffi_abi abi_type = FFI_MS_CDECL;
+        static constexpr ffi_abi abi_type =
+#       ifdef _WIN64
+            FFI_WIN64;
+#       else
+            FFI__MS_CDECL;
+#       endif
 
         template<typename Type>
         static std::string mangle_name(char const* funcName, char const* typeName)
@@ -137,6 +148,8 @@ namespace
             return buffer.str();
         }
     };
+
+#ifndef _WIN64
 
     template<class R, class... Args>
     struct function_traits<R(__stdcall *)(Args...)>
@@ -148,7 +161,8 @@ namespace
         static std::string mangle_name(char const* funcName, char const* typeName)
         {
             std::ostringstream buffer;
-            buffer << '_' << funcName << '_' << typeName << "__stdcall"
+            buffer << '_'
+                << funcName << '_' << typeName << "__stdcall"
                 << '@' << std::max<std::size_t>(8, sizeof(Type) * 2);
             return buffer.str();
         }
@@ -164,11 +178,14 @@ namespace
         static std::string mangle_name(char const* funcName, char const* typeName)
         {
             std::ostringstream buffer;
-            buffer << '@' << funcName << '_' << typeName << "__fastcall"
+            buffer << '@'
+                << funcName << '_' << typeName << "__fastcall"
                 << '@' << std::max<std::size_t>(8, sizeof(Type) * 2);
             return buffer.str();
         }
     };
+
+#endif
 
     template<class R, class... Args>
     struct function_traits<R(Args...)>
@@ -194,10 +211,15 @@ struct LibffiTestCase
     using function_type = Signature;
 };
 
+#ifdef _WIN64
+#   define ADD_FFI_TEST_CASES(Type)   \
+        Type (*)(Type, Type)
+#else
 #   define ADD_FFI_TEST_CASES(Type)   \
         Type (__cdecl *)(Type, Type),   \
         Type (__stdcall *)(Type, Type), \
         Type (__fastcall *)(Type, Type)
+#endif
 
 using LibffiTestTypes = ::testing::Types <
     ADD_FFI_TEST_CASES(char),
